62819f37fd39f1e74385f47a02d5d0356849028a
benhendriks <benhendriks@protonmail.com>
2023-09-25 22:03:25 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="h1-bold">Next JS, here we go!</h1>
+    </main>
+  )
+}
