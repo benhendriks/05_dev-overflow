f82a01d4073adada771fe338a8dc59bdeaad33c9
benhendriks <benhendriks@protonmail.com>
2023-09-26 07:19:55 +0200

    Added folder structur

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="h1-bold">Next JS, here we go!</h1>
+      <h2 className="h2-bold">Next JS, here we go!</h2>
+      <h3 className="h3-bold">Next JS, here we go!</h3>
     </main>
   )
 }
