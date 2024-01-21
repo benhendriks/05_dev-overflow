837152d076663a9981dca1f46a85ad32dfa7f52e
benhendriks <benhendriks@protonmail.com>
2024-01-21 19:36:53 +0100

    Added tag detail view and fixed question query

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,9 +1,11 @@
 'use server'
 
-import Tag from '@/database/tag.modal';
+import Question from '@/database/question.modal';
+import Tag, { ITag } from '@/database/tag.modal';
 import User from '@/database/user.modal';
+import { FilterQuery } from 'mongoose';
 import { connectToDatabase } from '../mongoose';
-import { GetTopInteractedTagsParams } from './shared.types';
+import { GetQuestionsByTagIdParams, GetTopInteractedTagsParams } from './shared.types';
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -16,12 +18,12 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
     // Find interactions for the User and group by tagId
 
     return [{ _id: '1', name: 'tag1' }, { _id: '2', name: 'tag2' }, { _id: '3', name: 'tag3' }]
-    if(!user) {
+    if (!user) {
       throw new Error('User not found')
     }
 
 
-  }catch (error){
+  } catch (error) {
     console.log("🚀 ~ file: tag.actions.ts:25 ~ getTopInteractedTags ~ error:", error)
     throw error;
   }
@@ -31,9 +33,45 @@ export async function getAllTags(params: any) {
   try {
     connectToDatabase();
     const tags = await Tag.find({});
-    return {tags}
-  }catch (error){
+    return { tags }
+  } catch (error) {
     console.log("🚀 ~ file: tag.actions.ts:36 ~ getAllTags ~ error:", error)
+    throw error;
+  }
+}
+
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+  try {
+    connectToDatabase();
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+    const tagFilter: FilterQuery<ITag> = { _id: tagId };
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: 'questions',
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: 'i'}}
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: '_id name' },
+        { path: 'author', model: User, select: '_id clerkId name picture' },
+      ]
+    });
+
+    if (!tag) {
+      throw new Error('Tag not found')
+    }
+
+    const questions = tag.questions;
+
+    return { tagTitle: tag.name, questions };
+
+  } catch (error) {
+    console.log("🚀 ~ file: tag.actions.ts:48 ~ getQuestionsByTagId ~ error:", error)
     throw error;
   }
 }
