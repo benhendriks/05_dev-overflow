91e03df7bd1595caf2755382bfd39806e0585c0e
benhendriks <benhendriks@protonmail.com>
2024-04-21 17:16:21 +0200

    Added local search for community & tags

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -86,8 +86,16 @@ export async function deleteUser(params: DeleteUserParams) {
 export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase();
-    const {page = 1, pageSize = 20, filter, searchQuery } = params;
-    const users = await User.find({})
+    const { searchQuery } = params;
+    const query: FilterQuery<typeof User> = {};
+
+    if(searchQuery) {
+      query.$or = [
+        { name: { $regex: new RegExp(searchQuery, 'i') } },
+        { username: {$regex: new RegExp(searchQuery, 'i') } },
+      ]
+    }
+    const users = await User.find(query)
     .sort({ createdAt: -1})
     return { users };
   } catch (error) {
