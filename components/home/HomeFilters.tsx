259c4fc2e118de0172e1b4fbde1287fee24cca25
benhendriks <benhendriks@protonmail.com>
2024-04-27 18:16:30 +0200

    Added manage filter state

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,11 +1,38 @@
 'use client'
 
-import React from 'react'
-import { Button } from '../ui/button'
 import { HomePageFilters } from '@/constants/filters'
+import { formUrlQuery } from '@/lib/utils'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import { Button } from '../ui/button'
 
 const HomeFilters = () => {
-  const active = 'newest';
+  const searchParams = useSearchParams();
+  const [active, setActive ] = useState('');
+  const router = useRouter();
+
+  const handleTypeClick = (item: string) => {
+    if(active == item) {
+      setActive('');
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: null
+      })
+      router.push( newUrl, { scroll: false });
+    } else {
+      setActive(item);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: item.toLowerCase()
+      })
+      router.push( newUrl, { scroll: false });
+    }
+  }
+
   return (
     <div className="flex-wrap hidden gap-3 mt-10 md:flex">
       {HomePageFilters.map((item:any) => (
@@ -15,6 +42,7 @@ const HomeFilters = () => {
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value
           ? 'bg-primary-100 text-primary-500'
           : 'bg-light-800 dark:text-light-500'}`}
+          onClickCapture={() => handleTypeClick(item.value)}
         >
           {item.name}
         </Button>
